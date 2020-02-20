a03f33c364e514c64dbf7e151c386069f771682e
shalala0115 <shalala0115@gmail.com>
2020-02-20 13:53:04 -0800

    Updates

---
+++ precache-manifest.d620af4efadff08d4227bddeeda975b7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e926687dda88a5033bf6e0ee3d9e3c",
+    "url": "/HW5/index.html"
+  },
+  {
+    "revision": "834c3bad6c0858eecb1f",
+    "url": "/HW5/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "698ab3a2ed3c55e7d7b3",
+    "url": "/HW5/static/css/main.e2deda98.chunk.css"
+  },
+  {
+    "revision": "834c3bad6c0858eecb1f",
+    "url": "/HW5/static/js/2.3e560d0a.chunk.js"
+  },
+  {
+    "revision": "1a606d213193971d68dc6ffbb605cb65",
+    "url": "/HW5/static/js/2.3e560d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698ab3a2ed3c55e7d7b3",
+    "url": "/HW5/static/js/main.0cb4b121.chunk.js"
+  },
+  {
+    "revision": "4081b1c1282a6b54786d",
+    "url": "/HW5/static/js/runtime-main.2e710ce1.js"
+  },
+  {
+    "revision": "4e75dffc8dcd0678f21f0ad281dd2193",
+    "url": "/HW5/static/media/header_icon.4e75dffc.png"
+  },
+  {
+    "revision": "f355c337b6313b09ddb14b966edf7e74",
+    "url": "/HW5/static/media/uni.f355c337.jpg"
+  }
+]);
